34ca51701c52f0794132bad42124621089b3d473
sandwich <yuzhoufengg@126.com>
2018-02-17 22:35:03 +0800

    自定义函数

---
+++ resource/pythonFunction.py
@@ -0,0 +1,17 @@
+print('abs(-90)=', abs(-90))
+print('max(90,-100)=', max(90,-100))
+a = int('90')
+print('类型转换int,int(\'90\')=', a)
+print('整数转16进制数', hex(9090))
+print('布尔值bool(1)', bool('1'))
+
+#自定义函数
+print('-------compare number-------')
+def compare (a, b) :
+	if a > b:
+		print(' a > b. a=%s,b=%s'%(a, b))
+	else :
+		print(' a <= b. a=%s,b=%s'%(a,b))
+
+print('definition function, compare result:',compare(9, 10))
+#definition function, compare result: None
